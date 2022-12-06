9065b6ad02fd5ed03425151e683f9293eead7c2c
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-06 12:32:05 +0300

    added ip mask

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,59 @@
-
+import React from "react";
+import MaskedInput from "react-text-mask";
 export default function Search() {
+  const props = {
+    guide: true,
+    mask: (value) => {
+      let result = [];
+      const chunks = value.split(".");
+
+      for (let i = 0; i < 4; ++i) {
+        const chunk = (chunks[i] || "").replace(/_/gi, "");
+
+        if (chunk === "") {
+          result.push(/\d/, /\d/, /\d/, ".");
+          continue;
+        } else if (+chunk === 0) {
+          result.push(/\d/, ".");
+          continue;
+        } else if (
+          chunks.length < 4 ||
+          (chunk.length < 3 && chunks[i].indexOf("_") !== -1)
+        ) {
+          if (
+            (chunk.length < 2 && +`${chunk}00` > 255) ||
+            (chunk.length < 3 && +`${chunk}0` > 255)
+          ) {
+            result.push(/\d/, /\d/, ".");
+            continue;
+          } else {
+            result.push(/\d/, /\d/, /\d/, ".");
+            continue;
+          }
+        } else {
+          result.push(...new Array(chunk.length).fill(/\d/), ".");
+          continue;
+        }
+      }
+
+      result = result.slice(0, -1);
+      return result;
+    },
+    pipe: (value) => {
+      if (value === "." || value.endsWith("..")) return false;
+
+      const parts = value.split(".");
+
+      if (
+        parts.length > 4 ||
+        parts.some((part) => part === "00" || part < 0 || part > 255)
+      ) {
+        return false;
+      }
+
+      return value;
+    },
+  };
   return (
     <>
       <div className="row mt-150 align-items-center">
@@ -7,7 +61,7 @@ export default function Search() {
           <p className="fs-1">IP Adress</p>
         </div>
         <div className="col-7 text-end">
-          
+          <MaskedInput {...props} />
         </div>
         <div className="col text-start">
           <button className="btn btn-primary">Search</button>
